fbc39a32cccfc8eb036f02de66e5656a3a5bdc48
yash-png <yashnerkar512@gmail.com>
2022-08-27 20:09:08 +0530

    Updates

---
+++ precache-manifest.ef236e50e01f19caa1699ed8485fb7db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4659f4370900b136f923",
+    "url": "/static/css/main.0cdedb66.chunk.css"
+  },
+  {
+    "revision": "4659f4370900b136f923",
+    "url": "/static/js/main.4659f437.chunk.js"
+  },
+  {
+    "revision": "11c4aba4207efa4b42e3",
+    "url": "/static/js/1.11c4aba4.chunk.js"
+  },
+  {
+    "revision": "ba450871a7c9e0ace408",
+    "url": "/static/js/2.ba450871.chunk.js"
+  },
+  {
+    "revision": "2397c5b165d0c8a14c8a",
+    "url": "/static/js/runtime~main.2397c5b1.js"
+  },
+  {
+    "revision": "6f62f33da0141355278bd22da558be32",
+    "url": "/index.html"
+  }
+];
